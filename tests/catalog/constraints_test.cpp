346d2cf16b7dd9a1ea35afb63781e861609f7b16
yudun <wyudun@gmail.com>
2016-03-18 14:22:24 -0400

    add constraints_test

---
+++ tests/catalog/constraints_test.cpp
@@ -0,0 +1,34 @@
+//
+// Created by yudun on 16/3/18.
+//
+
+//===----------------------------------------------------------------------===//
+//
+//                         PelotonDB
+//
+// catalog_test.cpp
+//
+// Identification: tests/catalog/constraints_test.cpp
+//
+// Copyright (c) 2016, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#include <cstdio>
+
+#include "harness.h"
+
+//#include "backend/catalog/catalog.h"
+
+namespace peloton {
+namespace test {
+
+//===--------------------------------------------------------------------===//
+// Constraints Tests
+//===--------------------------------------------------------------------===//
+
+class ConstraintsTests : public PelotonTest {};
+
+
+}  // End test namespace
+}  // End peloton namespace
